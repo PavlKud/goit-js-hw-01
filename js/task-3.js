dfc7b6dfb2628f502820197e6fc045bd6eadab78
PavlKud <144054118+PavlKud@users.noreply.github.com>
2023-11-18 18:10:40 +0100

    123

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,14 @@
 "use strict"
+function getElementWidth(content,padding,border){
+    // const isContent =   Number.parseFloat(content)
+    // const isPadding =  Number.parseFloat(padding * 2);
+    // const isBorder =  Number.parseFloat(border * 2); 
+
+    return content + 2 * padding + 2 * border;
+
+
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
